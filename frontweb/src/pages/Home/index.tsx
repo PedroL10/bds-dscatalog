cd9b21457c9a331dbd872b01e9739a5daa0cb73e
Pedro Luis <pepluisgon01@gmail.com>
2022-09-16 10:38:49 -0300

    Styling home page

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,8 +1,8 @@
-import { ReactComponent as MainImage} from 'assets/images/main-image.svg';
+import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import Navbar from 'components/Navbar';
 
-import './styles.css'
+import './styles.css';
 
 const Home = () => {
   return (
@@ -10,20 +10,23 @@ const Home = () => {
       <Navbar></Navbar>
       <div className="home-container">
         <div className="home-card">
-            <div className="home-content-container">
-                <h1>Conheca o melhor catalogo de produtos</h1>
-                <p>Ajudaremos voce a encontrar os melhores produtos disponiveis no mercado </p>
-                <ButtonIcon/>
+          <div className="home-content-container">
+            <div>
+              <h1>Conheca o melhor catalogo de produtos</h1>
+              <p>
+                Ajudaremos voce a encontrar os melhores produtos disponiveis no
+                mercado
+              </p>
             </div>
-            <div className="home-image-container">
-                <MainImage />
-            </div>
-
+            <ButtonIcon />
+          </div>
+          <div className="home-image-container">
+            <MainImage />
+          </div>
         </div>
-
       </div>
     </>
   );
-}
+};
 
 export default Home;
